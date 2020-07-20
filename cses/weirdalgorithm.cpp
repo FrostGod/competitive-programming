6b017cc7f93bc517fec39f1871bd9ddc58a431b3
redknight07 <divesh.chowdary@gmail.com>
2020-07-20 14:58:08 +0530

    some more problems added

---
+++ cses/weirdalgorithm.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h> 
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+#include<cmath>
+#include<string>
+#define pb push_back
+#define ll long long
+#define ull unsigned long long
+#define deb(x) cerr<<#x<<" "<<x<<"\n"
+#define debi cerr<<"hey sparky\n"
+#define x first
+#define y second
+#define fast ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define newline cout<<"\n"
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define INF (int)1e5+5
+#define nax (int)1e6+3
+#define eps (double)1e-9
+#define all(x) x.begin(),x.end()
+#define MAX_N (int)1e6+2
+//#define int long long
+using namespace std;
+using namespace __gnu_pbds; 
+typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> pdbs;
+ 
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+ 
+// look for  all edge cases
+//search for a pattern
+ 
+ 
+ 
+ 
+	
+signed main(){
+	fast;
+	ll n;
+	cin >> n;
+	while(n != 1){
+		cout<<n<<" ";
+		if(n%2 == 1){
+			n= n*3 + 1ll;
+		}
+		else{
+			n = n/2ll;
+		}
+		
+		if(n < 0){
+			break;
+		}
+	}
+	//cout<<n;
+	cout<<1<<" ";
+	
+	return 0;
+}
